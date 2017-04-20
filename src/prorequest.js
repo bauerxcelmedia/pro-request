8ea4ab1787df6311fc5aec13ef4ce369339ac121
Dean Burge <deburge@bauerxcelmedia.com.au>
2017-04-21 08:01:03 +1000

    allow all options pass through to request lib

--- src/prorequest.js
+++ src/prorequest.js
@@ -2,28 +2,19 @@ import request from 'request';
 import ExtendableError from 'es6-error';
 
 class RequestError extends ExtendableError {
-    constructor(method, url, json, statusCode, response) {
+    constructor(options, statusCode, response) {
         super('Http Request Error');
-        Object.assign(this, { method, url, json, statusCode, response });
+        Object.assign(this, { ...options, statusCode, response });
     }
 }
 
-function makeRequest(method, url, parameters) {
+function makeRequest(method, url, { headers = { 'Content-Type': 'application/json' }, ...parameters }) {
     return new Promise((resolve, reject) => {
-        let headers = { 'Content-Type': 'application/json' };
-        let json;
-
-        if (parameters) {
-            headers = parameters.headers;
-            json = parameters.json;
-        }
-
-        const options = { method, url, headers, json };
-
+        const options = { method, url, headers, ...parameters };
         request(options, (error, response, body) => {
             if (error) return reject(error);
             if (response.statusCode < 200 || response.statusCode >= 300) {
-                return reject(new RequestError(method, url, json, response.statusCode, body));
+                return reject(new RequestError(options, response.statusCode, body));
             }
             return resolve(response);
         });

796a7e3ec32125283140a9c3cfc7d36830a97ef3
Matt <matt.smith@digital.gov.au>
2018-04-16 10:29:27 +1000

    add prettier config

--- test/unit/prorequestSpec.js
+++ test/unit/prorequestSpec.js
@@ -20,54 +20,70 @@ describe('prorequest', () => {
             webRequestUtil = proxyquire('../../src/prorequest', stubs).default;
         });
 
-        it('should make an HTTP GET to a valid url and return the result', (done) => {
+        it('should make an HTTP GET to a valid url and return the result', done => {
             error = '';
             response = { statusCode: 200, body: { test: 'success' } };
             body = { test: 'success' };
 
-            const actual = webRequestUtil.get('http://test.com', { headers: { 'Content-Type': 'application/json' }, json: { test: 'test' } });
-
-            actual.then((res) => {
-                expect(res.body).to.deep.equal({ test: 'success' });
-                expect(res.statusCode).to.equal(200);
-                done();
-            }).catch((err) => {
-                done(err);
+            const actual = webRequestUtil.get('http://test.com', {
+                headers: { 'Content-Type': 'application/json' },
+                json: { test: 'test' }
             });
+
+            actual
+                .then(res => {
+                    expect(res.body).to.deep.equal({ test: 'success' });
+                    expect(res.statusCode).to.equal(200);
+                    done();
+                })
+                .catch(err => {
+                    done(err);
+                });
         });
 
-        it('should make an HTTP GET to a valid url and return an error', (done) => {
+        it('should make an HTTP GET to a valid url and return an error', done => {
             error = new Error('This is an error');
             response = { statusCode: 500, body: { test: 'fail' } };
             body = { test: 'fail' };
 
-            const actual = webRequestUtil.get('http://test.com', { headers: { 'Content-Type': 'application/json' }, json: { test: 'test' } });
-
-            actual.then(() => {
-            }).catch(() => {
-                expect(error.message).to.equal('This is an error');
-                done();
-                // This second catch is to catch any errors with the expect in the above catch
-            }).catch((err) => {
-                done(err);
+            const actual = webRequestUtil.get('http://test.com', {
+                headers: { 'Content-Type': 'application/json' },
+                json: { test: 'test' }
             });
+
+            actual
+                .then(() => {})
+                .catch(() => {
+                    expect(error.message).to.equal('This is an error');
+                    done();
+                    // This second catch is to catch any errors with the expect in the above catch
+                })
+                .catch(err => {
+                    done(err);
+                });
         });
 
-        it('should make an HTTP GET to a valid url and return an error when the response code is not a 200', (done) => {
+        it('should make an HTTP GET to a valid url and return an error when the response code is not a 200', done => {
             error = '';
             response = { statusCode: 500, body: { test: 'fail' } };
             body = { test: 'fail' };
 
-            const actual = webRequestUtil.get('http://test.com', { headers: { 'Content-Type': 'application/json' }, json: { test: 'test' } });
-
-            actual.then(() => {}).catch((err) => {
-                expect(err.statusCode).to.equal(500);
-                expect(err.response).to.equal(body);
-                done();
-                // This second catch is to catch any errors with the expect in the above catch
-            }).catch((err) => {
-                done(err);
+            const actual = webRequestUtil.get('http://test.com', {
+                headers: { 'Content-Type': 'application/json' },
+                json: { test: 'test' }
             });
+
+            actual
+                .then(() => {})
+                .catch(err => {
+                    expect(err.statusCode).to.equal(500);
+                    expect(err.response).to.equal(body);
+                    done();
+                    // This second catch is to catch any errors with the expect in the above catch
+                })
+                .catch(err => {
+                    done(err);
+                });
         });
     });
 
@@ -84,19 +100,24 @@ describe('prorequest', () => {
             webRequestUtil = proxyquire('../../src/prorequest', stubs).default;
         });
 
-        it('should make an HTTP POST to a valid url and return the result', (done) => {
+        it('should make an HTTP POST to a valid url and return the result', done => {
             error = '';
             response = { statusCode: 200, body: { test: 'success' } };
             body = { test: 'success' };
 
-            const actual = webRequestUtil.post('http://test.com', { headers: { 'Content-Type': 'application/json' }, json: { test: 'test' } });
-
-            actual.then((res) => {
-                expect(res.body).to.deep.equal({ test: 'success' });
-                done();
-            }).catch((err) => {
-                done(err);
+            const actual = webRequestUtil.post('http://test.com', {
+                headers: { 'Content-Type': 'application/json' },
+                json: { test: 'test' }
             });
+
+            actual
+                .then(res => {
+                    expect(res.body).to.deep.equal({ test: 'success' });
+                    done();
+                })
+                .catch(err => {
+                    done(err);
+                });
         });
     });
 
@@ -113,19 +134,24 @@ describe('prorequest', () => {
             webRequestUtil = proxyquire('../../src/prorequest', stubs).default;
         });
 
-        it('should make a request to a valid url and return the result', (done) => {
+        it('should make a request to a valid url and return the result', done => {
             error = '';
             response = { statusCode: 200, body: { test: 'success' } };
             body = { test: 'success' };
 
-            const actual = webRequestUtil.put('http://test.com', { headers: { 'Content-Type': 'application/json' }, json: { test: 'test' } });
-
-            actual.then((res) => {
-                expect(res.body).to.deep.equal({ test: 'success' });
-                done();
-            }).catch((err) => {
-                done(err);
+            const actual = webRequestUtil.put('http://test.com', {
+                headers: { 'Content-Type': 'application/json' },
+                json: { test: 'test' }
             });
+
+            actual
+                .then(res => {
+                    expect(res.body).to.deep.equal({ test: 'success' });
+                    done();
+                })
+                .catch(err => {
+                    done(err);
+                });
         });
     });
 
@@ -142,23 +168,28 @@ describe('prorequest', () => {
             webRequestUtil = proxyquire('../../src/prorequest', stubs).default;
         });
 
-        it('should make an HTTP DELETE to a valid url', (done) => {
+        it('should make an HTTP DELETE to a valid url', done => {
             response = { statusCode: 200, body: { test: 'deleted' } };
             body = { test: 'deleted' };
 
-            const actual = webRequestUtil.deleteRequest('http://test.com', { headers: { 'Content-Type': 'application/json' }, json: { test: 'deleted' } });
-
-            actual.then((res) => {
-                expect(res.body).to.deep.equal({ test: 'deleted' });
-                done();
-            }).catch((err) => {
-                done(err);
+            const actual = webRequestUtil.deleteRequest('http://test.com', {
+                headers: { 'Content-Type': 'application/json' },
+                json: { test: 'deleted' }
             });
+
+            actual
+                .then(res => {
+                    expect(res.body).to.deep.equal({ test: 'deleted' });
+                    done();
+                })
+                .catch(err => {
+                    done(err);
+                });
         });
     });
 
     describe('Request options passed', () => {
-        it('should pass through to request module', (done) => {
+        it('should pass through to request module', done => {
             error = '';
             response = { statusCode: 200, body: { test: 'success' } };
             body = { test: 'success' };
@@ -171,14 +202,19 @@ describe('prorequest', () => {
                 request: requestStub
             };
             webRequestUtil = proxyquire('../../src/prorequest', stubs).default;
-            const actual = webRequestUtil.get('http://test.com', { family: 4, json: { test: 'test' } });
-
-            actual.then(() => {
-                expect(exposedParams.family).to.equal(4);
-                done();
-            }).catch((err) => {
-                done(err);
+            const actual = webRequestUtil.get('http://test.com', {
+                family: 4,
+                json: { test: 'test' }
             });
+
+            actual
+                .then(() => {
+                    expect(exposedParams.family).to.equal(4);
+                    done();
+                })
+                .catch(err => {
+                    done(err);
+                });
         });
     });
 });
